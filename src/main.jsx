8af7bb83cf18209ea0753ccf8b8b0f025baadad1
reynold <reynold@nekro.dev>
2025-04-14 10:05:28 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
 
-
-const container = document.getElementById('root')
-
-if (container) {
-  const root = createRoot(container)
-
-  root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
-  )
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
-}
+    </Provider>
+  </React.StrictMode>,
+)
